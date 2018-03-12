646777e3d55a3e4b19eec0aea1d02f4a2e028557
mohsinht <l164333@lhr.nu.edu.pk>
2018-03-12 20:33:54 +0500

    debugged code6

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function sendText(sender, text){
 		method: "POST",
 		json: {
 			recipient: {id: sender},
-			message: messageData,
+			message: messageData
 		}
 	}, function(error, response, body){
 		if(error){
