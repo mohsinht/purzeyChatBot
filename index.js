07b0532327cd3f13545d36179cc96ad9fa702ba9
mohsinht <l164333@lhr.nu.edu.pk>
2018-03-12 19:54:00 +0500

    Enabling

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res){
 	res.send("Hello World! -Mohsin")
 })
 
-
+let token = "EAANqtvRqU5oBAGHtnynk1THZC1kXv0OYJjsZAwtSWHcBCSqhx2sGcZCNzhFP1vBUmaW8T5mTwnHaNWwZA25riQKZBBVaiCWwcV0qWpv9UyrHne9w7mMfx3zqhYC2GEVaZA3onUDyyAeUgv2ehnlBKDddEGHuccoXEtZAWybo37z0wZDZD"
 //facebook connect
 app.get('/webhook/', function(req, res){
 	if(req.query['hub.verify_token'] === 'mohsinhayat'){
@@ -26,6 +26,38 @@ app.get('/webhook/', function(req, res){
 	}
 	res.send("Wrong Token")
 })
+
+app.post('/webhook/', function(req, res){
+	let messaging_events = req.body.entry[0].messaging_events
+	for(let i = 0; i<messaging_events.length; i++){
+		let event = messaging_events[i]
+		let sender = event.sender.id
+		if(event.message && event.message.text){
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0, 100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text){
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/messages"
+		qs: {access_token, token},
+		method: "POST",
+		json: {
+			resipient: {id: sender},
+			message: messageData
+		}
+	}, function(error, response, body){
+		if(error){
+			console.log("sending error")
+		}else if(response.body.error){
+			console.log("response body error")
+		}
+	})
+}
 
 app.listen(app.get('port'), function(){
 	console.log("RUNNING: port")
