427c30c3eaef311c41d1d6465f9cc1d1e703d82c
mohsinht <l164333@lhr.nu.edu.pk>
2018-03-13 22:30:40 +0500

    updating phone

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/webhook/', function(req, res){
  			} 
 
  			const phNum = firstEntity(guess, 'phone_number');
-			if (phNum && phNum.confidence > 0.8) {
+			if (phNum && phNum.confidence > 0.9) {
 				let phn = text.substring(phNum.start, phNum.end)
     			sendText(sender, "We have noted down your Phone number. \n Your Phone Number = " + phn)
  			} 
