3ecd2ecaef21f47040aa71a999987de885c53319
mohsinht <l164333@lhr.nu.edu.pk>
2018-04-01 23:55:37 +0500

    asd

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.post('/webhook/', function(req, res){
 			    sendGenericMessage(sender)
 		    	continue
 		    }
+		    checkOrder(sender, text)
 			if(text.includes("profile")){
 				let profMsg = '';
 				if(userName!=null){
