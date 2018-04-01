182db28d58889e818a752b6082f6ecb1f9b931b6
mohsinht <l164333@lhr.nu.edu.pk>
2018-04-01 15:40:44 +0500

    asd

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.post('/webhook/', function(req, res){
 		}
 		if (event.postback) {
   	    	let text = JSON.stringify(event.postback)
-  	    	sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+  	    	sendText(sender, "Postback received: "+text.substring(0, 200), token)
   	    	continue
       	}
 		if(event.message && event.message.text){
