06bfeed57d690a857114dc2d815e842e10fc7a94
mohsinht <l164333@lhr.nu.edu.pk>
2018-04-02 23:00:03 +0500

    asd

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ app.post('/webhook/', function(req, res){
  				}
  				if(prID != 'noID'){
  					let prdINFO = getProduct(prID)
- 					while(prdINFO==null);
+ 					while(prdINFO.name==null);
  					sendText(sender, "The price of " + prdINFO.name + " is " + prdINFO.price + "PKR only.")
  				}
  				
