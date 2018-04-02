d8aa39832e2dda89b84a270947f63e0aad91cb30
mohsinht <l164333@lhr.nu.edu.pk>
2018-04-02 22:17:00 +0500

    asd

--- index.js
+++ index.js
@@ -136,27 +136,13 @@ app.post('/webhook/', function(req, res){
 
 
  			const prd = firstEntity(guess, 'product');
- 			prdInfo = 'empty';
- 			let prID = 'empty';
- 			if (prd && prd.confidence > 0.8){
+ 			 if (prd && prd.confidence > 0.8){
  			 	const prd_t = firstEntity(guess, 'product_type');
  			 	if(prd_t && prd_t.confidence > 0.8)
  				{
- 					if(prd.value == 'Handsfree'){
- 						if(prd_t.value == 'Samsung'){
- 							prID = "j5hf"
- 						}
- 						if(prd_t.value == 'AKG'){
- 							prID = "akghf"
- 						}
- 						if(prd_t.value == 'c7'){
- 							prID = "c7hf"
- 						}
- 					}
- 					if(prID!='empty'){
- 						prdInfo = getProduct(prID) 
- 						sendText(sender, "The price of " + prdInfo.name + " is " + prdInfo.price + "PKR only.")
- 						sendYesNo(sender, "Do you want to add it to your order list?", prID) 
+ 					if(prd.value == 'Handsfree' && prd_t.value == 'Samsung'){
+ 						sendText(sender, "The price of Samsung Handsfree is 70PKR only.")
+ 						sendYesNo(sender, "Do you want to add it to your order list?", "j5hf") 
  					}
  					//sendText(sender, "You talked about our product: " + prd_t.value + " " + prd.value)
  				}else{
