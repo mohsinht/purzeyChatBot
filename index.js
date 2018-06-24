ee5c1ea37cb42091b13eac4f2a947ec89a3a8b4e
mohsinht <l164333@lhr.nu.edu.pk>
2018-06-25 00:43:02 +0500

    asd

--- index.js
+++ index.js
@@ -98,6 +98,8 @@ app.post('/webhook/', function(req, res){
 						}else{
 							askUniversity(sender)
 						}
+					}else if(cuser.University.value === 'none'){
+						askMobileNumber(sender)
 					}
 				}
 			})		
@@ -167,7 +169,31 @@ function getDataFromDB(sender, child, data){
 }
 
 
-
+function askMobileNumber(sender){
+let messageData = {
+	     "text": "Apna mobile number enter kijiye, takay apsay contact krnay main asani ho.",
+	    "quick_replies":[
+	      {
+			"content_type":"user_phone_number"
+	      }
+	    ]
+	}
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+	    json: {
+		    recipient: {id:sender},
+		    message: messageData,
+	    }
+    }, function(error, response, body) {
+	    if (error) {
+		    console.log('Error sending messages: ', error)
+	    } else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
 
 
 function askUniversity(sender) {
