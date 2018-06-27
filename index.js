df9fa7a788378119b7525d88824c057f2b311689
mohsinht <l164333@lhr.nu.edu.pk>
2018-06-27 07:25:24 +0500

    asd

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.post('/webhook/', function(req, res){
       		sendMarkSeen(sender)
       		sendTypingOn(sender)
       		sendTypingOff(sender)
+      		sendText(sender, JSON.stringify(event.message))
       		let text = event.message.text.toLowerCase()
 			let guess = event.message.nlp
 			const intent = firstEntity(guess, 'intent')
