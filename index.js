77dc623df0303b108a14d6278d1a9350bfd32661
mohsinht <l164333@lhr.nu.edu.pk>
2018-08-28 20:00:57 +0500

    new

--- index.js
+++ index.js
@@ -330,20 +330,25 @@ app.post('/webhook/', function(req, res){
 						}
 						if(intent.value == 'asking_ca'){
 							var CAM = ""
+							var CAMphn = ""
 							if(cuser.University.value === "Fast-NU"){
 								CAM = "Mohsin Hayat"
+								CAMphn = "+923364256811"
 							}
 							if(cuser.University.value === "ITU"){
 								CAM = "Mubeen Ikram"
+								CAMphn = "+923331421741"
 							}
 							if(cuser.University.value === "COMSATS"){
 								CAM = "Khunshan Butt"
+								CAMphn = "+923214441444"
 							}
 							if(cuser.University.value === "PUCIT (New)"){
 								CAM = "Mustaghees Butt"
+								CAMphn = "+923164855152"
 							}
 							if(CAM !== ""){	
-								sendText(sender, "Aap ki university k Campus Ambassador " + CAM + " hain.")
+								sendCAMInfo(sender, CAM, CAMphn)
 							}
 							if(cuser.University.value === "none"){
 								sendText(sender, "Aap nay koi university select nai ki hui.")
@@ -732,6 +737,41 @@ function productOffer(sender, prd) {
 						"payload": "productOrder_" + prd.name
 					}],
 				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+
+function sendCAMInfo(sender, name, phone) {
+	let messageData = {
+		"attachment":{
+			"type":"template",
+			"payload":{
+				"template_type":"button",
+				"text":"Aapki University k campus ambassador " + name + " hain.\nPhone: " + phone + " 📞",
+				"buttons":[
+				{
+					"type":"phone_number",
+					"title":"Call " + name,
+					"payload":phone
+				}
+				]
 			}
 		}
 	}
